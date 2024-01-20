b9b852f80f828efe05d5261c819f895c35fc7376
aj392500 <117648889+aj392500@users.noreply.github.com>
2024-01-20 17:07:10 +0530

    Update lambda-cpu-util.py

--- lambda-cpu-util.py
+++ lambda-cpu-util.py
@@ -4,7 +4,8 @@ import json
 import boto3
 # Create a SSM client
 ssm_client = boto3.client('ssm')
- response = ssm_client.get_parameter(Name=sqlurl,WithDecryption=False)  # Decrypt if parameter is encrypted
+parameter_name ='arn:aws:ssm:ap-south-1:028093479336:parameter/sqsurl'
+ response = ssm_client.get_parameter(Name=parameter_name,WithDecryption=False)  # Decrypt if parameter is encrypted
 # Replace with your SQS queue URL
 queue_url = response['Parameter']['Value']
 # Create an SQS client
